76aef6839e2aae3ad0758a752ac932dbfb6691bc
kylekim4913 <77098871+kylekim4913@users.noreply.github.com>
2021-03-17 09:19:16 +0900

    push210317
    push

--- js/common.js
+++ js/common.js
@@ -1,24 +1,149 @@
 $(function(){
-    $('header').load('inc.html header .head',init);
+    $('header').load('inc.html header .head');
     $('.foot').load('inc.html .foot div');
 
-    
+    $.ajax({
+        url:'data.json',
+        success:function(dataC1){
 
-        function init(){
-            //헤시정보를 이용한 페이지 처리
-            var url = location.hash;
-            var pageNum = 0;
+            var type,imgSrc = '';
 
-/*             switch(url){
-                case '#0' : pageNum=0; break;
-                case '#1' : pageNum=0; break;
-                case '#2' : pageNum=0; break;
+            function dropImg(e){
+                tagList = '';
+                dataC1.photoGallery.forEach(function(v,k){
+                    if(v.type == e){
+
+                        type = dataC1.photoGallery[k].type;
+                        imgSrc = dataC1.photoGallery[k].imgSrc;
+
+                        tagList += "<li data type = "+ type + ">";
+                        tagList += "<a href='#'>";
+                        tagList += "<img src="+imgSrc+">";
+                        tagList += "</a></li>";
+                    }
+                })
+                $('.cmgallery .s1container div figure ul').html(tagList);
             }
- */        
-/*             $('.pcnav div a').on('click',function(){
-                localStorage.pageNum = $(this).index();
-            });
+            dropImg('pGallery');
 
-            $('.pcnav div a').eq(localStorage.pageNum).css({color:'#f00'}); */
-    }
+            //end of photoGallery
+
+            var title,author,date,viewCount = '';
+
+            function dropImg2(e){
+                tagList = '';
+                dataC1.daonAfter.forEach(function(v,k){
+                    if(v.type == e){
+
+                        type = dataC1.daonAfter[k].type;
+                        imgSrc = dataC1.daonAfter[k].imgSrc;
+                        title = dataC1.daonAfter[k].title;
+                        author = dataC1.daonAfter[k].author;
+                        date = dataC1.daonAfter[k].date;
+                        viewCount = dataC1.daonAfter[k].viewCount;
+
+                        tagList += "<li data type="+type+"><a href='#'><figure>",
+                        tagList += "<img src="+imgSrc+">",
+                        tagList += "<span></span><figcaption>",
+                        tagList += "<b>"+title+"</b>",
+                        tagList += "<p>작성자 <em>"+author+" </em>",
+                        tagList += "작성일 <em>"+date+"</em>",
+                        tagList += "조회 <em>"+viewCount+"</em>",
+                        tagList += "</p></figcaption></figure></a></li>"
+                    }
+                })
+                $('.cmgallery .spotcardview').html(tagList);
+            }
+            dropImg2('dAfter');
+        }//end of success
+
+    })//end of ajax
+
+    $('header')
+
+    $(window).on('scroll',function(e){
+
+        var i = $(window).scrollTop();
+        console.log(i)
+        if(i>700){
+            $('.txtbox').addClass('active');
+        }
+        if(i>1500){
+            $('.txtbanner').addClass('active');
+        }
+        if(i>2400){
+            $('.spotafter div h2').addClass('active');
+            $('.spotafter div p').addClass('active');
+        }
+        if(i>2500){
+            $('.spotcardview').addClass('active');
+        }
+    })//end of scroll
+
+    $('.intro a').on('click',function(){
+        $('.ir_popup').addClass('active')
+    })
+    $('.ir_popup').on('click',function(){
+        $('.ir_popup').removeClass('active')
+    })
+
+    var fadeNum = 0;
+    $('.sr_bg').not(':first').hide(); 
+    setInterval(function(){ 
+        $('.sr_bg').eq(fadeNum).fadeOut(1000); 
+        fadeNum++;
+        if(fadeNum == 2){fadeNum=0}
+        $('.sr_bg').eq(fadeNum).fadeIn(1000);
+    },4000);//end of slide
+
+    var fadeNumm =0;
+    $('.sv_bg').not(':first').hide(); 
+    setInterval(function(){ 
+        $('.sv_bg').eq(fadeNumm).fadeOut(1000); 
+        fadeNumm++;
+        if(fadeNumm == 2){fadeNumm=0}
+        $('.sv_bg').eq(fadeNumm).fadeIn(1000);
+    },4000);//end of slide
+
+    $('#au1room_thumb li a').on('click',function(){
+        var th = $(this).children().attr('src');
+        
+        
+        $('#au1room_img img').attr('src',th);
+    })
+    $('#au1room_thumb li').on('click',function(){
+        
+        var order = $(this).index();
+        console.log(order)
+        $('#au1roomNum').text('20'+(order+2)+' 호')
+        
+    })
+    $('#au2room_thumb li a').on('click',function(){
+        var th = $(this).children().attr('src');
+        
+        
+        $('#au2room_img img').attr('src',th);
+    })
+    $('#au2room_thumb li').on('click',function(){
+        
+        var order = $(this).index();
+        console.log(order)
+        $('#au2roomNum').text('20'+(order+2)+' 호')
+        
+    })//end of attr img
+
+    $(window).on('scroll',function(e){
+
+        var i = $(window).scrollTop();
+        console.log(i)
+        if(i>1000){
+            $('#au2room_img').addClass('active');
+        }
+        if(i>1000){
+            $('#au2room_box').addClass('active');
+        }
+        if(i>1000){
+            $('#au2room_thumb').addClass('active');
+        }
+    })//end of scroll
 });
